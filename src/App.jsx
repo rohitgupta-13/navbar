7fb0d80ead71317a1dd86371c61679f18f3bb34c
Rohit <rohitgupta2823@gmail.com>
2026-02-17 21:46:20 +0530

    fix case-sensitive imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from './components/Hero'
-import { About } from './components/About'
+import About  from './components/About'
 import Courses from './components/Courses'
 
 const App = () => {
